d4728b71f1d6255fcd259497168e72d307f1613e
Darker3424 <alessander2907@gmail.com>
2022-04-09 21:59:00 -0500

    initial commit

---
+++ LIBDATOS/ClsApli01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBDATOS
+{
+    public class ClsApli01
+    {
+        static VentasIDATEntities db = new VentasIDATEntities();
+        public static Array ListarPedidos()
+        {       
+            var query = from p in db.Pedidos select p;
+            return query.ToArray();//ToArray un tipo arreglo
+        }
+        public static Array PedidosporRangoFechas(DateTime FechaInicio,DateTime FechaFinal)
+        {
+            var query=from p in db.Pedidos join e in db.Empleados on
+            p.IdEmpleado equals e.IdEmpleado join d in db.Detalles_de_pedidos on
+            p.IdPedido equals d.IdPedido
+            where p.FechaPedido >= FechaInicio && p.FechaPedido <= FechaFinal
+            group new {p,e,d}
+            by new {p.IdPedido,e.IdEmpleado,p.FechaPedido,e.Nombre,e.Apellidos} into GD
+            select new
+            {
+                IdPedido = GD.Key.IdPedido, IdEmpleado= GD.Key.IdEmpleado,
+                Fecha_Pedido= GD.Key.FechaPedido,
+                Empleado= GD.Key.Apellidos+"  "+ GD.Key.Nombre,
+                Venta= Math.Round(GD.Sum(g =>
+                (double)g.d.PrecioUnidad*g.d.Cantidad*(1-g.d.Descuento)))                
+            };
+            return query.ToArray();
+        }
+    }
+}
---
+++ LabSesionEFLIBCLASES/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LIBDATOS;
+
+namespace LabSesionEFLIBCLASES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //dataGridView1.DataSource = ClsApli01.ListarPedidos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(dtpFechaInicio.Value > dtpFechaFinal.Value)
+            {
+                MessageBox.Show("La fecha de inicio es mayor a la fecha final");
+            }
+            else
+            {
+                dataGridView1.DataSource = ClsApli01.PedidosporRangoFechas(
+                    dtpFechaInicio.Value, dtpFechaFinal.Value);
+                dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+        }
+    }
+}
